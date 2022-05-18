5aa527bacdd823c728ba9e4ada4d88adc477ef32
Harprit Singh <harp.singh@gmail.com>
2022-05-19 00:27:16 +0530

    Lot of changes

--- PSTop.ps1
+++ PSTop.ps1
@@ -1,33 +1,42 @@
-function Show-Meter ($percentage){
-    $display = "["
-    1..$percentage | ForEach-Object { $display += "|" }
-    $percentage..100 | ForEach-Object { $display += " " }
-    $display += "]"
-    return $display
+function Show-Bar ($percentage){
+    Write-Host "[" -NoNewline
+    $display = ""
+    if ($percentage -eq 0){
+        1..100 | ForEach-Object { $display += " " }
+        Write-Host $display -NoNewline
+    }
+    else{
+        if ($percentage -lt 70){
+            $bar_color = "Green"
+        }
+        elseif ($percentage -lt 90){
+            $bar_color = "Yellow"
+        }
+        else{
+            $bar_color = "Red"
+        }
+        1..$percentage | ForEach-Object { $display += "|" }
+        $percentage..99 | ForEach-Object { $display += " " }
+        Write-Host $display -ForegroundColor $bar_color -NoNewline
+    }
+    Write-Host "]" -NoNewline
 }
 
-$total_memory = [Math]::Round((Get-WmiObject -Class win32_operatingsystem -Property TotalVisibleMemorySize).TotalVisibleMemorySize / 1MB)
+$total_memory = [Math]::Round((Get-WmiObject -Class win32_operatingsystem -Property TotalVisibleMemorySize).TotalVisibleMemorySize / 1MB, 2)
 
 while ($True){
     $cpu_avg = [Math]::Round((Get-Counter -Counter '\Processor(_Total)\% Processor Time').CounterSamples.CookedValue, 0)
-    $used_memory = [Math]::Round((Get-Counter -Counter '\Memory\Available Bytes').CounterSamples[0].CookedValue / 1GB)
+    $available_memory = [Math]::Round((Get-Counter -Counter '\Memory\Available Bytes').CounterSamples[0].CookedValue / 1GB, 2)
+    $used_memory = $total_memory - $available_memory
+    $processes = Get-Process | Sort-Object CPU -Descending | Select-Object -First 10 | Format-Table
 
     [Console]::Clear()
 
-    # $display_text = "CPU: "
-    # $display_text += Show-Meter $cpu_avg
-    # $display_text += " $cpu_avg%"
-    # $display_text += "`n"
-    # $display_text += "Mem: "
-    # $display_text += Show-Meter $(($used_memory / $total_memory) * 100)
-    # $display_text += " $used_memory GB / $total_memory GB"
-    # $display_text += "`n"
-    # Write-Host $display_text
-
-    [Console]::Write("CPU: ")
-    [Console]::Write($(Show-Meter $cpu_avg))
-    [Console]::WriteLine(" $cpu_avg%")
-    [Console]::Write("Mem: ")
-    [Console]::Write($(Show-Meter $(($used_memory / $total_memory) * 100)))
-    [Console]::WriteLine(" $used_memory GB / $total_memory GB")
+    Write-Host "CPU: " -NoNewline
+    Show-Bar $cpu_avg
+    Write-Host " $cpu_avg%"
+    Write-Host "Mem: " -NoNewline
+    Show-Bar $(($used_memory / $total_memory) * 100)
+    Write-Host " $used_memory GB / $total_memory GB"
+    $processes
 }
